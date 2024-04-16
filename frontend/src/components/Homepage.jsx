4ef4819699f92eb6ab19c8a24c2fc9e0cfffe634
codeUWE <code.cayan@gmail.com>
2024-04-16 14:35:14 +0200

    homepage and dark mode correction in different routes

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,5 +1,168 @@
+import { useNavigate } from 'react-router-dom';
+
+//assets images
+import PokemonLogo from '../assets/pokemonLogo.png';
+import iconArceus from '../assets/iconArceus.png';
+import iconCelebi from '../assets/iconCelebi.png';
+import iconDeoxys from '../assets/iconDeoxys.png';
+import iconMew from '../assets/iconMew.png';
+import glurak from '../assets/glurak.png';
+import pikachu from '../assets/pikachu.png';
+import stern from '../assets/stern.png';
+import grassBackground from '../assets/grassBackground.png';
+
+import { Card, Button } from 'flowbite-react';
+
 function Homepage() {
-	return <div></div>;
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<div className="relative flex flex-col justify-center items-center bg-gradient-to-b from-blue-500 via-orange-100 to-yellow-100">
+				<img
+					src={glurak}
+					alt=""
+					className="absolute top-0 right-0 z-10"
+					width={500}
+				/>
+				<img
+					src={pikachu}
+					alt=""
+					className="absolute top-96 left-0 z-10"
+					width={300}
+				/>
+				<div className="w-full flex justify-center items-center">
+					<div className="w-[400px] h-[600px] bg-gradient-to-b from-yellow-300 via-yellow-200 to-yellow-300 rounded-xl flex flex-col items-center justify-center shadow-zinc-700 shadow-md m-4 z-10 ">
+						<div
+							className={`flex flex-col items-center bg-gradient-to-b from-violet-300 via-fuchsia-200 to-violet-300 `}
+							style={{
+								width: '370px',
+								height: '570px',
+								borderRadius: '0.375rem',
+							}}
+						>
+							<div>
+								<div className="flex justify-between items-center mt-2">
+									<h3 className="bg-gray-50 bg-opacity-85 px-2 rounded-3xl">
+										1000000 HP
+									</h3>
+									<div className="flex gap-1">
+										<div
+											className={`w-6 h-6 p-1 rounded-full bg-gradient-to-b from-red-600 via-red-400 to-red-600 flex justify-center items-center border-2 border-black shadow-xl`}
+										>
+											<img src={stern} alt="Star Icon" width={18} />
+										</div>
+										<div
+											className={`w-6 h-6 p-1 rounded-full bg-gradient-to-b from-green-600 via-green-400 to-green-600 flex justify-center items-center border-2 border-black shadow-xl`}
+										>
+											<img src={stern} alt="Star Icon" width={18} />
+										</div>
+										<div
+											className={`w-6 h-6 p-1 rounded-full bg-gradient-to-b from-blue-600 via-blue-400 to-blue-600 flex justify-center items-center border-2 border-black shadow-xl`}
+										>
+											<img src={stern} alt="Star Icon" width={18} />
+										</div>
+										<div
+											className={`w-6 h-6 p-1 rounded-full bg-gradient-to-b from-violet-600 via-violet-400 to-violet-600 flex justify-center items-center border-2 border-black shadow-xl`}
+										>
+											<img src={stern} alt="Star Icon" width={18} />
+										</div>
+										<div
+											className={`w-6 h-6 p-1 rounded-full bg-gradient-to-b from-yellow-500 via-yellow-400 to-yellow-500 flex justify-center items-center border-2 border-black shadow-xl`}
+										>
+											<img src={stern} alt="Star Icon" width={18} />
+										</div>
+									</div>
+								</div>
+								<div className="relative w-[340px] h-[300px] rounded-sm mx-auto mt-2 bg-cover bg-center border-[3.5px] border-gray-300 bg-blue-500 items-center overflow-hidden">
+									<img
+										src={PokemonLogo}
+										alt="Pokemon Logo"
+										className="absolute top-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+									/>
+									<img
+										src="https://tmpfiles.nohat.cc/m2i8N4N4K9i8K9d3.png"
+										alt="Decorative Background"
+										className="absolute top-24 left-[75px] w-48 h-48 object-cover z-0"
+									/>
+								</div>
+							</div>
+
+							<div className=" relative w-[340px] h-[170px] border-2 border-zinc-800 mx-auto mt-3 flex justify-center items-center">
+								<img
+									src={iconMew}
+									alt="Mew"
+									className="absolute top-0 left-0"
+									width={60}
+								/>
+								<img
+									src={iconArceus}
+									alt="Arceus"
+									className="absolute bottom-0 left-0"
+									width={50}
+								/>
+								<img
+									src={iconCelebi}
+									alt="Celebi"
+									className="absolute top-0 right-0"
+									width={60}
+								/>
+								<img
+									src={iconDeoxys}
+									alt="Deoxys"
+									className="absolute bottom-0 right-0"
+									width={50}
+								/>
+								<h3 className="text-2xl font-bold">
+									Gotta catch &apos;em all!
+								</h3>
+							</div>
+						</div>
+					</div>
+					<main className="z-20">
+						<div>
+							<Card className="w-[400px] h-[400px] z-40 border-2">
+								<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+									Welcome to <br /> this Pokemon Fanpage.
+								</h5>
+								<p className="font-normal text-gray-700 dark:text-gray-400 mt-4">
+									You can view important information about the first{' '}
+									<span className="font-bold text-red-700 dark:text-purple-400">
+										809
+									</span>{' '}
+									Pokemon over here! Although you can visit the Pokemon Arena to
+									battle other Pokemon with the Pokemon of your choice! Just
+									click on the buttons bellow.
+								</p>
+
+								<Button
+									pill
+									className="bg-red-700 mt-8"
+									onClick={() => navigate('/arena')}
+								>
+									Visit Arena
+								</Button>
+								<Button
+									pill
+									className="bg-blue-700 mt-3"
+									onClick={() => navigate('/pokemon')}
+								>
+									See Pokemon
+								</Button>
+							</Card>
+						</div>
+					</main>
+				</div>
+				<div>
+					<img
+						src={grassBackground}
+						alt=""
+						className="absolute top-40 left-0 z-0"
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Homepage;
--- frontend/src/components/SinglePokemon.jsx
+++ frontend/src/components/SinglePokemon.jsx
@@ -56,200 +56,219 @@ function SinglePokemon() {
 
 	return (
 		<>
-			<div className="w-1/2 mx-auto flex justify-center">
-				<Breadcrumb
-					aria-label="Default breadcrumb example"
-					className="cursor-pointer rounded-2xl px-6 py-2 border-2"
-				>
-					<Breadcrumb.Item onClick={() => navigate('/')}>Home</Breadcrumb.Item>
-					<Breadcrumb.Item onClick={() => navigate('/pokemon')}>
-						All Pokemon
-					</Breadcrumb.Item>
-					<Breadcrumb.Item
-						onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
+			<div className="dark:bg-sky-950">
+				<div className="w-1/2 mx-auto flex justify-center">
+					<Breadcrumb
+						aria-label="Default breadcrumb example"
+						className="cursor-pointer rounded-2xl px-6 py-2 border-2 mt-4"
 					>
-						{singlePokemon?.name?.english}
-					</Breadcrumb.Item>
-				</Breadcrumb>
-			</div>
-			<div className="w-full flex justify-center items-center">
-				<div className="w-[400px] h-[600px] bg-gradient-to-b from-gray-500 via-gray-300 to-gray-500 rounded-xl flex flex-col items-center justify-center shadow-zinc-700 shadow-xl m-12 ">
-					<div
-						className={`flex flex-col items-center ${getCardBackground()}`}
-						style={{
-							width: '370px',
-							height: '570px',
-							borderRadius: '0.375rem',
-						}}
-					>
-						{singlePokemon?.id && (
-							<div>
-								<div className="flex justify-between items-center mt-2">
-									<div className="format">
-										<h3>{singlePokemon.name.english}</h3>
-									</div>
-									<div className="flex gap-2">
-										<h3 className="bg-gray-50 bg-opacity-85 px-2  rounded-3xl">
-											{singlePokemon.base.HP} HP
-										</h3>
-										<div
-											className={`w-6 h-6 p-1 rounded-full ${getCardBackground()} flex justify-center items-center border-2 border-black shadow-xl`}
-										>
-											<img src={Star} alt="Star Icon" width={18} />
-										</div>
-										{singlePokemon.type[1] ? (
-											<div
-												className={`w-6 h-6 p-1 rounded-full ${getStarBackground()} flex justify-center items-center border-2 border-black shadow-md`}
-											>
-												<img src={Star} alt="Star Icon" width={18} />
-											</div>
-										) : (
-											''
-										)}
-									</div>
-								</div>
-								<div
-									style={{
-										backgroundImage: `url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${singlePokemon.id}.png)`,
-									}}
-									className="w-[340px] h-[300px] rounded-sm mx-auto mt-2 bg-cover bg-center border-[3.5px] border-gray-300 bg-white bg-opacity-85"
-								></div>
-							</div>
-						)}
-
-						<div className="w-[340px] h-[170px] border-2 border-zinc-800 mx-auto mt-3 format ps-2 flex flex-col justify-between items-center p-4">
-							<h2 className="m-0 ">
-								#{singlePokemon?.id ?? 'Loading...'} from{' '}
-								{singlePokemon?.id && pokedexRegion()}
-							</h2>
-							<h4> Click Buttons for detailed information</h4>
-							<div className="flex space-x-2">
-								<Button
-									onClick={() => navigate(`/pokemon/${singlePokemon.id}/name`)}
-									className="bg-zinc-800 text-orange-200 px-4 rounded-3xl border-none"
-								>
-									Name
-								</Button>
-								<Button
-									onClick={() => navigate(`/pokemon/${singlePokemon.id}/type`)}
-									className="bg-zinc-800 text-pink-300 px-4 rounded-3xl border-none"
-								>
-									Type
-								</Button>
-								<Button
-									onClick={() => navigate(`/pokemon/${singlePokemon.id}/base`)}
-									className="bg-zinc-800 text-blue-300  px-4 rounded-3xl border-none"
-								>
-									Base
-								</Button>
-							</div>
-						</div>
-					</div>
+						<Breadcrumb.Item
+							onClick={() => navigate('/')}
+							className="dark:text-white"
+						>
+							Home
+						</Breadcrumb.Item>
+						<Breadcrumb.Item
+							onClick={() => navigate('/pokemon')}
+							className="dark:text-white"
+						>
+							All Pokemon
+						</Breadcrumb.Item>
+						<Breadcrumb.Item
+							onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
+							className="dark:text-white"
+						>
+							<span className="text-blue-700 dark:text-red-500 font-bold">
+								{singlePokemon?.name?.english}
+							</span>
+						</Breadcrumb.Item>
+					</Breadcrumb>
 				</div>
-				{!info && (
-					<>
-						<div className="w-[400px] h-80 flex flex-col items-center gap-5 p-4 format">
-							<h2 className="mt-20">
-								Hello User, click on the buttons, <br /> to display different
-								information about{' '}
-								<span className="text-blue-700">
-									{singlePokemon?.name.english}
-								</span>
-							</h2>
-						</div>
-					</>
-				)}
-				{info === 'name' && singlePokemon && (
-					<>
-						<div className="flex flex-col items-center gap-5">
-							<div className="w-[400px] h-80 bg-gray-200 rounded-3xl format p-6 flex flex-col justify-around shadow-xl">
-								<h2>
-									{singlePokemon.name.english}&rsquo;s Name in different
-									languages:
-								</h2>
-								<div className="px-4 py-2 rounded-xl bg-orange-300 bg-opacity-80">
-									<h3>English: {singlePokemon.name.english}</h3>
-									<h3>Japanese: {singlePokemon.name.japanese}</h3>
-									<h3>Chinese: {singlePokemon.name.chinese}</h3>
-									<h3>French: {singlePokemon.name.french}</h3>
-								</div>
-							</div>
-							<Button
-								pill
-								className="mt-2 bg-blue-700"
-								onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
-							>
-								Show Card only
-							</Button>
-						</div>
-					</>
-				)}
-				{info === 'type' && singlePokemon && (
-					<>
-						<div className="flex flex-col items-center gap-5">
-							<div className="w-[400px] h-80 bg-gray-200 rounded-3xl format p-6 flex flex-col justify-around shadow-xl">
-								<h2>Types of {singlePokemon.name.english}:</h2>
-								<div className="mt-8">
-									<div className="bg-pink-300 bg-opacity-80 px-4 py-2 rounded-xl">
-										<div className="flex gap-2 items-center">
+				<div className="w-full flex justify-center items-center">
+					<div className="w-[400px] h-[600px] bg-gradient-to-b from-yellow-300 via-yellow-200 to-yellow-300  rounded-xl flex flex-col items-center justify-center shadow-zinc-700 shadow-xl m-12 ">
+						<div
+							className={`flex flex-col items-center ${getCardBackground()}`}
+							style={{
+								width: '370px',
+								height: '570px',
+								borderRadius: '0.375rem',
+							}}
+						>
+							{singlePokemon?.id && (
+								<div>
+									<div className="flex justify-between items-center mt-2">
+										<div className="format">
+											<h3>{singlePokemon.name.english}</h3>
+										</div>
+										<div className="flex gap-2">
+											<h3 className="bg-gray-50 bg-opacity-85 px-2  rounded-3xl">
+												{singlePokemon.base.HP} HP
+											</h3>
 											<div
 												className={`w-6 h-6 p-1 rounded-full ${getCardBackground()} flex justify-center items-center border-2 border-black shadow-xl`}
 											>
 												<img src={Star} alt="Star Icon" width={18} />
 											</div>
-
-											<h3 className="mt-2">{singlePokemon.type[0]}</h3>
+											{singlePokemon.type[1] ? (
+												<div
+													className={`w-6 h-6 p-1 rounded-full ${getStarBackground()} flex justify-center items-center border-2 border-black shadow-md`}
+												>
+													<img src={Star} alt="Star Icon" width={18} />
+												</div>
+											) : (
+												''
+											)}
 										</div>
-										{singlePokemon.type[1] ? (
+									</div>
+									<div
+										style={{
+											backgroundImage: `url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${singlePokemon.id}.png)`,
+										}}
+										className="w-[340px] h-[300px] rounded-sm mx-auto mt-2 bg-cover bg-center border-[3.5px] border-gray-300 bg-white bg-opacity-85"
+									></div>
+								</div>
+							)}
+
+							<div className="w-[340px] h-[170px] border-2 border-zinc-800 mx-auto mt-3 format ps-2 flex flex-col justify-between items-center p-4">
+								<h2 className="m-0 ">
+									#{singlePokemon?.id ?? 'Loading...'} from{' '}
+									{singlePokemon?.id && pokedexRegion()}
+								</h2>
+								<h4> Click Buttons for detailed information</h4>
+								<div className="flex space-x-2">
+									<Button
+										onClick={() =>
+											navigate(`/pokemon/${singlePokemon.id}/name`)
+										}
+										className="bg-zinc-800 text-orange-200 px-4 rounded-3xl border-none"
+									>
+										Name
+									</Button>
+									<Button
+										onClick={() =>
+											navigate(`/pokemon/${singlePokemon.id}/type`)
+										}
+										className="bg-zinc-800 text-pink-300 px-4 rounded-3xl border-none"
+									>
+										Type
+									</Button>
+									<Button
+										onClick={() =>
+											navigate(`/pokemon/${singlePokemon.id}/base`)
+										}
+										className="bg-zinc-800 text-blue-300  px-4 rounded-3xl border-none"
+									>
+										Base
+									</Button>
+								</div>
+							</div>
+						</div>
+					</div>
+					{!info && (
+						<>
+							<div className="w-[400px] h-80 flex flex-col items-center gap-5 p-4 format">
+								<h2 className="mt-20 dark:text-gray-300">
+									Hello User, click on the buttons, <br /> to display different
+									information about{' '}
+									<span className="text-blue-700 dark:text-red-500">
+										{singlePokemon?.name.english}
+									</span>
+								</h2>
+							</div>
+						</>
+					)}
+					{info === 'name' && singlePokemon && (
+						<>
+							<div className="flex flex-col items-center gap-5">
+								<div className="w-[400px] h-80 bg-gray-200 rounded-3xl format p-6 flex flex-col justify-around shadow-xl">
+									<h2>
+										{singlePokemon.name.english}&rsquo;s Name in different
+										languages:
+									</h2>
+									<div className="px-4 py-2 rounded-xl bg-orange-300 bg-opacity-80">
+										<h3>English: {singlePokemon.name.english}</h3>
+										<h3>Japanese: {singlePokemon.name.japanese}</h3>
+										<h3>Chinese: {singlePokemon.name.chinese}</h3>
+										<h3>French: {singlePokemon.name.french}</h3>
+									</div>
+								</div>
+								<Button
+									pill
+									className="mt-2 bg-blue-700"
+									onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
+								>
+									Show Card only
+								</Button>
+							</div>
+						</>
+					)}
+					{info === 'type' && singlePokemon && (
+						<>
+							<div className="flex flex-col items-center gap-5">
+								<div className="w-[400px] h-80 bg-gray-200 rounded-3xl format p-6 flex flex-col justify-around shadow-xl">
+									<h2>Types of {singlePokemon.name.english}:</h2>
+									<div className="mt-8">
+										<div className="bg-pink-300 bg-opacity-80 px-4 py-2 rounded-xl">
 											<div className="flex gap-2 items-center">
 												<div
-													className={`w-6 h-6 p-1 rounded-full ${getStarBackground()} flex justify-center items-center border-2 border-black shadow-xl`}
+													className={`w-6 h-6 p-1 rounded-full ${getCardBackground()} flex justify-center items-center border-2 border-black shadow-xl`}
 												>
 													<img src={Star} alt="Star Icon" width={18} />
 												</div>
 
-												<h3 className="mt-2">{singlePokemon.type[1]}</h3>
+												<h3 className="mt-2">{singlePokemon.type[0]}</h3>
 											</div>
-										) : (
-											''
-										)}
+											{singlePokemon.type[1] ? (
+												<div className="flex gap-2 items-center">
+													<div
+														className={`w-6 h-6 p-1 rounded-full ${getStarBackground()} flex justify-center items-center border-2 border-black shadow-xl`}
+													>
+														<img src={Star} alt="Star Icon" width={18} />
+													</div>
+
+													<h3 className="mt-2">{singlePokemon.type[1]}</h3>
+												</div>
+											) : (
+												''
+											)}
+										</div>
 									</div>
 								</div>
+								<Button
+									pill
+									className="mt-2 bg-blue-700"
+									onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
+								>
+									Show Card only
+								</Button>
 							</div>
-							<Button
-								pill
-								className="mt-2 bg-blue-700"
-								onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
-							>
-								Show Card only
-							</Button>
-						</div>
-					</>
-				)}
-				{info === 'base' && singlePokemon && (
-					<>
-						<div className="flex flex-col items-center gap-5">
-							<div className="w-[400px] h-80 bg-gray-200 rounded-3xl format p-6 flex flex-col justify-around shadow-xl">
-								<h2>Base Attributes:</h2>
-								<div className="bg-blue-300 bg-opacity-80 rounded-xl px-4 py-2">
-									<h4>HP: {singlePokemon.base.HP}</h4>
-									<h4>Attack: {singlePokemon.base.Attack}</h4>
-									<h4>Defense: {singlePokemon.base.Defense}</h4>
-									<h4>Sp. Attack: {singlePokemon.base['Sp. Attack']}</h4>
-									<h4>Sp. Defense: {singlePokemon.base['Sp. Defense']}</h4>
-									<h4>Speed: {singlePokemon.base.Speed}</h4>
+						</>
+					)}
+					{info === 'base' && singlePokemon && (
+						<>
+							<div className="flex flex-col items-center gap-5">
+								<div className="w-[400px] h-80 bg-gray-200 rounded-3xl format p-6 flex flex-col justify-around shadow-xl">
+									<h2>Base Attributes:</h2>
+									<div className="bg-blue-300 bg-opacity-80 rounded-xl px-4 py-2">
+										<h4>HP: {singlePokemon.base.HP}</h4>
+										<h4>Attack: {singlePokemon.base.Attack}</h4>
+										<h4>Defense: {singlePokemon.base.Defense}</h4>
+										<h4>Sp. Attack: {singlePokemon.base['Sp. Attack']}</h4>
+										<h4>Sp. Defense: {singlePokemon.base['Sp. Defense']}</h4>
+										<h4>Speed: {singlePokemon.base.Speed}</h4>
+									</div>
 								</div>
+								<Button
+									pill
+									className="mt-2 bg-blue-700"
+									onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
+								>
+									Show Card only
+								</Button>
 							</div>
-							<Button
-								pill
-								className="mt-2 bg-blue-700"
-								onClick={() => navigate(`/pokemon/${singlePokemon?.id} `)}
-							>
-								Show Card only
-							</Button>
-						</div>
-					</>
-				)}
+						</>
+					)}
+				</div>
 			</div>
 		</>
 	);
